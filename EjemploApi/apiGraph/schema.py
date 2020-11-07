044116caaa25d4e8f2f55a2259fa13c20159d230
Hernan <hagarciag@gmail.com>
2020-11-07 11:12:53 -0500

    Query specific clients and multiple requests at once.

--- EjemploApi/apiGraph/schema.py
+++ EjemploApi/apiGraph/schema.py
@@ -10,6 +10,10 @@ class ClienteType(DjangoObjectType):
 # Unlike REST which requires a standard and static signature, graphQL define queries and mutations
 class Query(graphene.ObjectType):
     all_clientes=graphene.List(ClienteType)
+    some_clientes=graphene.List(ClienteType, num_doc=graphene.String(), tipo_num_doc=graphene.String())
 
     def resolve_all_clientes(self, info, **kwargs):
         return Cliente.objects.all()
+
+    def resolve_some_clientes(self, info, **kwargs):
+        return Cliente.objects.filter(num_doc=kwargs.get('num_doc'), tipo_num_doc=kwargs.get('tipo_num_doc'))
